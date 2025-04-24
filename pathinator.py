18621a4f72b97fb16f63445f9a922a21da4774f6
Simone Sangiorgio <simone.sangiorgio2000@gmail.com>
2025-04-24 21:54:13 +0200

    Update pathinator.py

--- pathinator.py
+++ pathinator.py
@@ -6,7 +6,7 @@ base_path = Path(__file__).resolve().parent
 
 dataset_path = base_path / "dataset"
 
-train_path = dataset_path / "train.csv"
+train_path = dataset_path / "train_augmented.csv"
 
 validation_path = dataset_path / "valid.csv"
 
